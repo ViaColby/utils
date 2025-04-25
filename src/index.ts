66698cb33ffd4aedcca9f60eee4d70845ca21e30
HYY <13880134230@163.com>
2025-04-25 15:27:36 +0800

    feat: add measureText && Scheduler

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,6 @@ export { findBranches } from './findBranches';
 export { findNodes } from './findNodes';
 
 export { measureText } from './measureText';
+
+export { Scheduler } from './scheduler';
+export type { Task } from './scheduler';
